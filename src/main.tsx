b6088f37e8d7cfd216b42d343e2ec952c53d4441
Isaac <kch062522@gmail.com>
2025-02-09 21:34:08 +0800

    feat: wrap App in query client provider

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,17 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import { ThemeProvider } from "styled-components";
 import * as theme from "./theme";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./libs/tanstack-query.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </Provider>
+    </QueryClientProvider>
   </StrictMode>
 );
