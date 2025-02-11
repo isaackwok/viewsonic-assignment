1df883b503a21f775c819c90d4a7df1c745bf1be
Isaac Kwok <isaac.kwok@utonia.com>
2025-02-11 18:02:09 +0800

    feat: memoize StudentCard when render

--- src/feature/classroom/student-score-board/StudentListTab.tsx
+++ src/feature/classroom/student-score-board/StudentListTab.tsx
@@ -1,16 +1,19 @@
 import { Grid } from "./shared";
 import { StudentCard } from "./StudentCard";
 import { Student } from "../../../types/student";
+import { memo } from "react";
 
 type StudentListTabProps = {
   students: Student[];
 };
 
+const MemoizedStudentCard = memo(StudentCard);
+
 export function StudentListTab({ students }: StudentListTabProps) {
   return (
     <Grid>
       {students.map((student, idx) => (
-        <StudentCard
+        <MemoizedStudentCard
           key={student.id}
           name={student.name}
           number={idx + 1}
